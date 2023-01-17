3a9c086564309a30aa83d4c4838ba97d52cba197
Ben <Benmosborn2006@gmail.com>
2023-01-17 18:50:51 +0000

    Change it to 20

--- MainGameplay.py
+++ MainGameplay.py
@@ -19,7 +19,7 @@ class resizableWindowUpdate:
     def __init__(self):
         self.windowSize = pygame.display.get_window_size()
         self.scaleWVduDimensionsX = (int(self.windowSize[0]) / 500) * 20
-        self.scaleWVduDimensionsY = (int(self.windowSize[1]) / 400) * 20
+        self.scaleWVduDimensionsY = (int(self.windowSize[1]) / 400) * 10
     def update(self):
         newscaleWVduDimensionsX = (int(self.windowSize[0]) / 500) * 20
         newscaleWVduDimensionsY = (int(self.windowSize[1]) / 400) * 20

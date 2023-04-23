c60fa87835c6df9e50f3e15b8e5e5754295d2f56
George Osborn <georgeosborn2002@gmail.com>
2023-04-23 14:37:29 +0100

    Remove Menus folder, as it is not needed

--- Menus/StartScreenController.py
+++
@@ -1,7 +0,0 @@
-class StartScreenController:
-    def goToMainMenu():
-        print("goToMainMenu")
-        # gameStates.mainMenuState == True
-
-    def pickPlayerProfile():
-        print("pickPlayerProfile")

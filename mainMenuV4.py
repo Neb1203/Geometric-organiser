fd31a4f91b8dd18402b0fedeeb0b9eb5f65f70fc
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-04-24 12:26:12 +0100

    Simplified code and added player profile menu and main menu

--- mainMenuV4.py
+++ mainMenuV4.py
@@ -1,43 +1,57 @@
 import pygame
 import pygame_menu
 from Window import Window
-# import MainGameplay
-from pygame_menu.examples import create_example_window
-
-from typing import Tuple, Any, Optional, List
+from MainGameplay import MainGameplay
 
 
 mainTheme = pygame_menu.themes.THEME_BLUE
 window = Window()
-
 def set_difficulty(value, difficulty):
-    # Do the job here !
+    # Do the job here
     print(difficulty)
 
 def startGame():
     # Do the job here !
     # MainGameplay()
     pass
-surface: Optional['pygame.Surface'] = None
-surface = create_example_window('Example - Game Selector', window.vduDimensions)
 
-startScreen = pygame_menu.Menu('Start Screen', 400, 300,
+surface = window.setMode
+
+startScreen = pygame_menu.Menu('Start Screen',
+                               window.vduDimensions[0],
+                               window.vduDimensions[1],
                             theme=mainTheme)
-playMenu = pygame_menu.Menu('Play', 400, 300,
+playMenu = pygame_menu.Menu('Play',
+                            window.vduDimensions[0],
+                            window.vduDimensions[1],
+                            theme=mainTheme)
+playerProfile = pygame_menu.Menu('Change player Profiles',
+                            window.vduDimensions[0],
+                            window.vduDimensions[1],
+                            theme=mainTheme)
+mainMenu = pygame_menu.Menu('Main Menu',
+                            window.vduDimensions[0],
+                            window.vduDimensions[1],
                             theme=mainTheme)
 
 #create buttons for startScreen
-startScreen.add.text_input('Name :', default='Insert Name here')
-startScreen.add.button('Go to main menu', playMenu)
+startScreen.add.label("Geometric Organiser")
+startScreen.add.button('Go to main menu', mainMenu)
+startScreen.add.button('Change player profile', playerProfile)
 startScreen.add.button('Quit', pygame_menu.events.EXIT)
+
+#create buttons for mainMenu
+mainMenu.add.button('Play', playMenu)
+mainMenu.add.button('Back', pygame_menu.events.BACK)
 
 #Create buttons for playMenu
 playMenu.add.selector('Difficulty :', [('Hard', 1), ('Normal', 2), ('Easy', 3)], onchange=set_difficulty)
 playMenu.add.button('Start Game', startGame())
-# playMenu.add.button('back; #go back a menu
+playMenu.add.button('back', pygame_menu.events.BACK)
 
+#buttons for player profiles
+playerProfile.add.text_input('Name :', default='Insert Name here')
+playerProfile.add.button('back', pygame_menu.events.BACK)
 
 if startScreen.is_enabled():
     startScreen.mainloop(surface)
-if playMenu.is_enabled():
-    playMenu.mainloop(surface)

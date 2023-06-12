93882715eadd58b1de225b426d79502e9b6fc48c
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-06-12 09:23:15 +0100

    Re commited code

--- database/API/databaseConnect.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector
-class databaseConnector:
-    cnx = mysql.connector.connect(host='127.0.0.1', port='3306', user='neb120', password='Bentofon12',
-                                  database='geometricDatabase')
-    cursor = cnx.cursor()

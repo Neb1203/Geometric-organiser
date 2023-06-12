4662de3087e82c0e50c427367bd26a3ea70e1f2d
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-06-12 09:22:37 +0100

    Made a sign up screen and got api pings to work

--- mainMenuV4.py
+++ mainMenuV4.py
@@ -3,40 +3,70 @@ import pygame_menu
 from MainGameplay import MainGameplay
 from Window import Window
 import requests
+import pprint
+from hashGenerator import HashingGenerator
 
 subTheme = pygame_menu.themes.THEME_SOLARIZED
 
 mainTheme = pygame_menu.themes.THEME_ORANGE
 window = Window()
+hashGenerator = HashingGenerator()
+
 subWindow = (300,200)
 
-def setDifficulty(value, difficulty):
-    # Do the job here
-    # Window(50)
-    pass
 
-def screenSize(screenSize):
-    Window.newScreenSize = screenSize
 
-def userName(userName):
-    user_Name = userName
+class menuOptions:
+    def __init__(self):
+        self.user_email = 'null'
+        self.user_password = 'null'
+        self.user_name = 'null'
+    def setDifficulty(self, value, difficulty):
+        # Do the job here
+        # Window(50)
+        pass
 
-def email(email):
-    pass
-def pickGameMode(value, gameMode):
-    global gameModeSelected
-    gameModeSelected = gameMode
-def startGame():
-    # Do the job here !
-    if gameModeSelected == False:
-        MainGameplay()
-    elif gameModeSelected == True:
-        print("cum")
-def confirmProfile(userName):
-    payload = {'james'}
-    r = requests.post('127.0.0.2:5000/playerDetails', params=payload)
+    def screenSize(self, screenSize):
+        # Window.newScreenSize = screenSize
+        pass
+
+    def userName(self, userName):
+        self.user_name = userName
+
+    def password(self, password):
+        self.user_password = password
+
+
+    def email(self, email):
+        self.user_email = email
+
+    def pickGameMode(self, value, gameMode):
+        global gameModeSelected
+        gameModeSelected = gameMode
+    def startGame(self):
+        # Do the job here !
+        if gameModeSelected == False:
+            MainGameplay()
+        elif gameModeSelected == True:
+            print("cum")
+
+    def confirmProfile(self):
+        print(self.user_email, self.user_password)
+        signup = {'email': self.user_email, 'password': self.user_password, 'user_name': self.user_name}
+
+        r = requests.post('http://127.0.0.2:5000/player_details', params=signup)
+
+        # payload = {'james'}
+        # r = requests.post('127.0.0.2:5000/playerDetails', params=payload)
+    def profileCreation():
+        pass
+menuOptions = menuOptions()
 surface = window.surface
 
+signup = pygame_menu.Menu('signup Screen',
+                               window.vduDimensions[0],
+                               window.vduDimensions[1],
+                            theme=mainTheme)
 startScreen = pygame_menu.Menu('Start Screen',
                                window.vduDimensions[0],
                                window.vduDimensions[1],
@@ -57,11 +87,12 @@ mainMenu = pygame_menu.Menu('Main Menu',
                             window.vduDimensions[0],
                             window.vduDimensions[1],
                             theme=mainTheme)
-profileCreation = pygame_menu.Menu('Create a profile',
-                                   subWindow[0],
-                                   subWindow[1],
-                                   theme=subTheme)
 
+#signup screen
+signup.add.text_input('User name :', copy_paste_enable=True, onchange=menuOptions.userName)
+signup.add.text_input('Email :', copy_paste_enable=True, onchange=menuOptions.email)
+signup.add.text_input('Password :', copy_paste_enable=True, onchange=menuOptions.password)
+signup.add.button('Signup', menuOptions.confirmProfile)
 #create buttons for startScreen
 startScreen.add.label("Geometric Organiser")
 startScreen.add.button('Go to main menu', mainMenu)
@@ -77,25 +108,25 @@ mainMenu.add.button('Back', pygame_menu.events.BACK)
 settings.add.range_slider('Set screen size',
                           range_values=[1,2,3,4,5],
                           default=3,
-                          onchange=screenSize)
+                          onchange = menuOptions.screenSize)
 
 #Create buttons for playMenu
-playMenu.add.selector('Gamemode : ', [('Campaign', True), ('Endless', False)], onchange = pickGameMode)
-playMenu.add.selector('Difficulty :', [('Hard', 1), ('Normal', 2), ('Easy', 3)], onchange = setDifficulty)
-playMenu.add.button('Start Game', startGame)
+playMenu.add.selector('Gamemode : ', [('Campaign', True), ('Endless', False)], onchange = menuOptions.pickGameMode)
+playMenu.add.selector('Difficulty :', [('Hard', 1), ('Normal', 2), ('Easy', 3)], onchange = menuOptions.setDifficulty)
+playMenu.add.button('Start Game', menuOptions.startGame)
 playMenu.add.button('back', pygame_menu.events.BACK)
 
 #buttons for player profiles
-playerProfile.add.button('Create a profile', profileCreation)
+playerProfile.add.button('Create a profile', menuOptions.profileCreation)
 playerProfile.add.button('back', pygame_menu.events.BACK)
 
 #buttons for profile creation
-profileCreation.add.text_input('User Name :', onchange=confirmProfile)
-profileCreation.add.text_input('Email :', onchange=email)
-profileCreation.add.button('confirm', confirmProfile)
+# profileCreation.add.text_input('User Name :', onchange=confirmProfile)
+# profileCreation.add.text_input('Email :', onchange=email)
+# profileCreation.add.button('confirm', confirmProfile)
 
 #world map
 
 
-if startScreen.is_enabled():
-    startScreen.mainloop(surface)
+if signup.is_enabled():
+    signup.mainloop(surface)

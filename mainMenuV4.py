f2ced21c54c2f75b4778c759c946fb17420a8ead
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-06-23 10:05:56 +0100

    Made function to register when the menu pages changed. Made it so the userName is displayed on the mainMenu

--- mainMenuV4.py
+++ mainMenuV4.py
@@ -3,24 +3,27 @@ import pygame_menu
 from MainGameplay import MainGameplay
 from Window import Window
 import requests
-import json
 from hashGenerator import HashingGenerator
-
+# pygame.mixer.init()
+# pygame.mixer.music.load('mmm.mp3')
+# pygame.mixer.music.play(-1, 0)
 subTheme = pygame_menu.themes.THEME_SOLARIZED
+mainTheme = pygame_menu.themes.Theme(background_color=(177, 94, 108),
+                  title_background_color=(252, 100, 88),
+                                     title_font_shadow=True,
+                  title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
+                  widget_font=pygame_menu.font.FONT_8BIT)
 
-mainTheme = pygame_menu.themes.THEME_ORANGE
 window = Window()
 hashGenerator = HashingGenerator()
-
 subWindow = (300,200)
-
-
 
 class menuOptions:
     def __init__(self):
         self.user_email = 'null'
         self.user_password = 'null'
         self.user_name = 'null'
+        self.user_token = 'null'
     def setDifficulty(self, value, difficulty):
         # Do the job here
         # Window(50)
@@ -32,14 +35,13 @@ class menuOptions:
 
     def userName(self, userName):
         self.user_name = userName
+        update_label_widget()
 
     def password(self, password):
         self.user_password = password
 
     def email(self, email):
         self.user_email = email
-
-
     def pickGameMode(self, value, gameMode):
         global gameModeSelected
         gameModeSelected = gameMode
@@ -51,14 +53,19 @@ class menuOptions:
             print("cum")
 
     def signup(self):
+        # menuChanged()
+        # mainMenu.set_current()
+        print("def signup running")
         signup = {'email': self.user_email, 'password': self.user_password, 'user_name': self.user_name}
-
-        r = requests.post('http://127.0.0.2:5000/player_details', params=signup)
+        self.response = requests.post('http://127.0.0.2:5000/player_details', params=signup)
+        labelDisplay().labelDisplay(self.user_name)
 
     def login(self):
+        print("def login running")
         login = {'email': self.user_email, 'password': self.user_password}
 
         response = requests.get('http://127.0.0.2:5000/player_details', params=login)
+
         if response.status_code == 200:
             try:
                 response_content = response.content.decode('utf-8')  # Decode the response content from bytes to a string
@@ -71,13 +78,11 @@ class menuOptions:
                     if isinstance(jsonResponse, dict):
                         user_name = jsonResponse.get('username')
                         print("Extracted username:", user_name)
-
-                        if user_name is not None: # Pass the extracted username to the read function
-                            self.read(self.user_email, self.user_password)
-                        else: # Handle the case when the username is None
-                            pass
+                        self.user_token = jsonResponse.get('token')
+                        print("Extracted token:", self.user_token)
+                        return self.user_token
                     else: # Handle the case when the response is not a JSON object
-                        pass
+                        print("The data isn't in JSON")
             except ValueError: # Handle the case when the response content cannot be parsed as JSON
                 pass
         else: # Handle the case when the request was not successful
@@ -85,51 +90,75 @@ class menuOptions:
 
     def profileCreation():
         pass
+
+def update_label_widget():
+    print('def update_label_widget', menuOptions.user_name)
+
+
 menuOptions = menuOptions()
 surface = window.surface
 
-signup = pygame_menu.Menu('signup Screen',
-                               window.vduDimensions[0],
-                               window.vduDimensions[1],
-                            theme=mainTheme)
-login = pygame_menu.Menu('Login screen',
-                               window.vduDimensions[0],
-                               window.vduDimensions[1],
-                            theme=mainTheme)
-startScreen = pygame_menu.Menu('Start Screen',
-                               window.vduDimensions[0],
-                               window.vduDimensions[1],
-                            theme=mainTheme)
-playMenu = pygame_menu.Menu('Play',
-                            window.vduDimensions[0],
-                            window.vduDimensions[1],
-                            theme=mainTheme)
-settings = pygame_menu.Menu('settings',
-                            window.vduDimensions[0],
-                            window.vduDimensions[1],
-                            theme=mainTheme)
-playerProfile = pygame_menu.Menu('Change player Profiles',
-                            window.vduDimensions[0],
-                            window.vduDimensions[1],
-                            theme=mainTheme)
-mainMenu = pygame_menu.Menu('Main Menu',
-                            window.vduDimensions[0],
-                            window.vduDimensions[1],
-                            theme=mainTheme)
+signup = pygame_menu.Menu('signup Screen', window.vduDimensions[0], window.vduDimensions[1], theme=mainTheme)
+login = pygame_menu.Menu('Login screen', window.vduDimensions[0], window.vduDimensions[1], theme=mainTheme)
+startScreen = pygame_menu.Menu('Start Screen', window.vduDimensions[0], window.vduDimensions[1], theme=mainTheme)
+playMenu = pygame_menu.Menu('Play', window.vduDimensions[0], window.vduDimensions[1], theme=mainTheme)
+settings = pygame_menu.Menu('settings', window.vduDimensions[0], window.vduDimensions[1], theme=mainTheme)
+playerProfile = pygame_menu.Menu('Change player Profiles', window.vduDimensions[0], window.vduDimensions[1], theme=mainTheme)
+mainMenu = pygame_menu.Menu('Main Menu', window.vduDimensions[0], window.vduDimensions[1], theme=mainTheme)
+
+signupState = False
+loginState = False
+def menuChanged(current, menu):
+    global signupState
+    global loginState
+    if menu == 'signup':
+        print('signUp')
+        signupState = True
+    elif menu == 'login':
+        print('login')
+        loginState = True
+    elif menu == 'startScreen':
+        print('startscreen')
+    elif menu == 'playMenu':
+        print('playMenu')
+    elif menu == 'settings':
+        print('settings')
+    elif menu == 'playerProfile':
+        print('playerProfile')
+    elif menu == 'mainMenu':
+        print('mainMenu')
+        if signupState == True:
+            menuOptions.signup()
+            loginState = False
+            signupState = False
+        elif loginState == True:
+            menuOptions.login()
+            loginState = False
+            signupState = False
+
+signup.set_onbeforeopen(lambda current, menu: menuChanged(current, 'signup'))
+login.set_onbeforeopen(lambda current, menu: menuChanged(current, 'login'))
+startScreen.set_onbeforeopen(lambda current, menu: menuChanged(current, 'startScreen'))
+playMenu.set_onbeforeopen(lambda current, menu: menuChanged(current, 'playMenu'))
+settings.set_onbeforeopen(lambda current, menu: menuChanged(current, 'settings'))
+playerProfile.set_onbeforeopen(lambda current, menu: menuChanged(current, 'playerProfile'))
+mainMenu.set_onbeforeopen(lambda current, menu: menuChanged(current, 'mainMenu'))
 
 #signup screen
 signup.add.text_input('User name :', copy_paste_enable=True, onchange=menuOptions.userName)
 signup.add.text_input('Email :', copy_paste_enable=True, onchange=menuOptions.email)
 signup.add.text_input('Password :', copy_paste_enable=True, password=True, onchange=menuOptions.password)
-signup.add.button('Signup', onselect = menuOptions.signup)
-
+signupButton = signup.add.button('Signup', mainMenu)
 signup.add.button('Back', pygame_menu.events.BACK)
+
 #login screen
 login.add.text_input('Email :', copy_paste_enable=True, onchange=menuOptions.email)
 login.add.text_input('Password :', copy_paste_enable=True, password=True, onchange=menuOptions.password)
-login.add.button('Login', menuOptions.login)
+loginButton = login.add.button('Login', mainMenu)
+loginButton.set_onselect(menuOptions.login)
 
 login.add.button('back', pygame_menu.events.BACK)
+
 
 #create buttons for startScreen
 startScreen.add.label("Geometric Organiser")
@@ -142,6 +171,9 @@ startScreen.add.button('Quit', pygame_menu.events.EXIT)
 mainMenu.add.button('Play', playMenu)
 mainMenu.add.button('Settings', settings)
 mainMenu.add.button('Back', pygame_menu.events.BACK)
+class labelDisplay:
+    def labelDisplay(self, userName):
+        user_label = mainMenu.add.label(userName)
 
 #settings
 settings.add.range_slider('Set screen size',
@@ -159,9 +191,9 @@ playMenu.add.button('back', pygame_menu.events.BACK)
 playerProfile.add.button('Create a profile', signup)
 playerProfile.add.button('back', pygame_menu.events.BACK)
 
-
 #world map
 
 
-if startScreen.is_enabled():
-    startScreen.mainloop(surface)
+if __name__ == "__main__":
+    if startScreen.is_enabled():
+        startScreen.mainloop(surface)

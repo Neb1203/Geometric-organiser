1c3a2bb9f696e8f6ac1536b4d640c4804f03ae31
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-07-07 13:07:09 +0100

    Changed background colour to match menu colour (pink)

--- Colours.py
+++ Colours.py
@@ -2,5 +2,5 @@ class Colours:
     def __init__(self):
         self.gray = (128, 128, 128)
         self.black = (0, 0, 0)
-        self.backgroundColour = (255, 154, 0)
+        self.backgroundColour = (255, 166, 158)
         self.gridColour = self.gray

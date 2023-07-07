14fdd360ba080c585f3f57c826ab02d9768d3e82
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-07-07 17:33:51 +0100

    Created pause menu render

--- MainGameplay.py
+++ MainGameplay.py
@@ -6,14 +6,15 @@ from Window import Window
 from Figure import Figure
 from GamePaused import GamePaused
 from GameStateEnum import GameStateEnum
+import button
 
 class MainGameplay:
     def __init__(self):
-        window = Window()
+        w = Window()
         colours = Colours()
 
-        scaleWVduDimensionsX = (int(window.vduDimensions[0]) / 500) * 20
-        scaleWVduDimensionsY = (int(window.vduDimensions[1]) / 400) * 20
+        scaleWVduDimensionsX = (int(w.vduDimensions[0]) / 500) * 20
+        scaleWVduDimensionsY = (int(w.vduDimensions[1]) / 400) * 20
 
         g = GamePaused()
 
@@ -84,32 +85,50 @@ class MainGameplay:
                 if event.key == pygame.K_a or pygame.K_LEFT:
                     pressing_left = False
 
-            window.surface.fill(colours.backgroundColour)
+            w.surface.fill(colours.backgroundColour)
 
             for i in range(game.height):
                 for j in range(game.width):
-                    pygame.draw.rect(window.surface, colours.gridColour, [game.x + scaleWVduDimensionsX * j, game.y + scaleWVduDimensionsY * i, scaleWVduDimensionsX, scaleWVduDimensionsY], 1)
+                    pygame.draw.rect(w.surface, colours.gridColour, [game.x + scaleWVduDimensionsX * j, game.y + scaleWVduDimensionsY * i, scaleWVduDimensionsX, scaleWVduDimensionsY], 1)
                     if game.field[i][j] > 0:
-                        pygame.draw.rect(window.surface, Figure.colors[game.field[i][j]],
+                        pygame.draw.rect(w.surface, Figure.colors[game.field[i][j]],
                                          [game.x + scaleWVduDimensionsX * j + 1, game.y + scaleWVduDimensionsY * i + 1, scaleWVduDimensionsX - 2, scaleWVduDimensionsY - 1])
             if game.figure is not None:
                 for i in range(4):
                     for j in range(4):
                         p = i * 4 + j
                         if p in game.figure.image():
-                            pygame.draw.rect(window.surface, Figure.colors[game.figure.color],
+                            pygame.draw.rect(w.surface, Figure.colors[game.figure.color],
                                              [game.x + scaleWVduDimensionsX * (j + game.figure.x) + 1,
                                               game.y + scaleWVduDimensionsY * (i + game.figure.y) + 1,
                                               scaleWVduDimensionsX - 2, scaleWVduDimensionsY - 2])
 
             fontOpenSans = pygame.font.SysFont('sans', 35)
             score_tracker = fontOpenSans.render("Score: " + str(game.score), True, colours.black)
+            pauseResumeButton = fontOpenSans.render("Resume", True, colours.black)
 
-
-            window.surface.blit(score_tracker, [0, 0])
-            if game.state == "gameover":
+            w.surface.blit(score_tracker, [0, 0])
+            if game.state.gameOver():
                 print("gameState = gameOver")
+            if game.state.paused():
+                square_color = (255, 255, 255)  # Red color
+                square_size = 200
+                square_x = w.vduDimensions[0] // 2 - square_size // 2  # Center the square horizontally
+                square_y = w.vduDimensions[1] // 2 - square_size // 2 - 60
 
+                pygame.draw.rect(w.surface, square_color, (square_x, square_y, square_size, square_size))
+
+                resume_img = pygame.image.load("button images/resumeButton (Custom).png").convert_alpha()
+                mainMenuImage = pygame.image.load("button images/mainMenuButton (Custom).png").convert_alpha()
+
+                # create button instances
+                resume_button = button.Button(304, 160, resume_img, 1)
+                mainMenuButton = button.Button(304, 250, mainMenuImage, 1)
+
+                if resume_button.draw(w.surface):
+                    game.state = GameStateEnum.STARTED
+                if mainMenuButton.draw(w.surface):
+                    print("Return to main menu button pressed")
             pygame.display.flip()
             clock.tick(fps)
 

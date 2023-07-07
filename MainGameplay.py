5eb0c13dedffef9d0b70d7c853caf15e6925b4a6
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-07-07 13:09:32 +0100

    Removed usage of resizableWindow.py. Removed original text displayed in game

--- MainGameplay.py
+++ MainGameplay.py
@@ -1,5 +1,5 @@
 import pygame
-
+import pygame_menu
 from Colours import Colours
 from GridDraw import Tetris
 from Window import Window
@@ -10,6 +10,9 @@ class MainGameplay:
     def __init__(self):
         window = Window()
         colours = Colours()
+
+        scaleWVduDimensionsX = (int(window.vduDimensions[0]) / 500) * 20
+        scaleWVduDimensionsY = (int(window.vduDimensions[1]) / 400) * 20
 
         resizableWindowUpdateVar = resizableWindowUpdate()
 
@@ -64,7 +67,7 @@ class MainGameplay:
                     if event.key == pygame.K_SPACE:
                         game.goSpace()
                     if event.key == pygame.K_ESCAPE:
-                        game.__init__(10, 20)
+                        pygame_menu.events.BACK
 
                     if event.key == pygame.K_p:
                         pygame.quit()
@@ -81,35 +84,27 @@ class MainGameplay:
 
             for i in range(game.height):
                 for j in range(game.width):
-                    pygame.draw.rect(window.surface, colours.gridColour, [game.x + resizableWindowUpdateVar.scaleWVduDimensionsX * j, game.y + resizableWindowUpdateVar.scaleWVduDimensionsY * i, resizableWindowUpdateVar.scaleWVduDimensionsX, resizableWindowUpdateVar.scaleWVduDimensionsY], 1)
+                    pygame.draw.rect(window.surface, colours.gridColour, [game.x + scaleWVduDimensionsX * j, game.y + scaleWVduDimensionsY * i, scaleWVduDimensionsX, scaleWVduDimensionsY], 1)
                     if game.field[i][j] > 0:
                         pygame.draw.rect(window.surface, Figure.colors[game.field[i][j]],
-                                         [game.x + resizableWindowUpdateVar.scaleWVduDimensionsX * j + 1, game.y + resizableWindowUpdateVar.scaleWVduDimensionsY * i + 1, resizableWindowUpdateVar.scaleWVduDimensionsX - 2, resizableWindowUpdateVar.scaleWVduDimensionsY - 1])
+                                         [game.x + scaleWVduDimensionsX * j + 1, game.y + scaleWVduDimensionsY * i + 1, scaleWVduDimensionsX - 2, scaleWVduDimensionsY - 1])
             if game.figure is not None:
                 for i in range(4):
                     for j in range(4):
                         p = i * 4 + j
                         if p in game.figure.image():
                             pygame.draw.rect(window.surface, Figure.colors[game.figure.color],
-                                             [game.x + resizableWindowUpdateVar.scaleWVduDimensionsX * (j + game.figure.x) + 1,
-                                              game.y + resizableWindowUpdateVar.scaleWVduDimensionsY * (i + game.figure.y) + 1,
-                                              resizableWindowUpdateVar.scaleWVduDimensionsX - 2, resizableWindowUpdateVar.scaleWVduDimensionsY - 2])
+                                             [game.x + scaleWVduDimensionsX * (j + game.figure.x) + 1,
+                                              game.y + scaleWVduDimensionsY * (i + game.figure.y) + 1,
+                                              scaleWVduDimensionsX - 2, scaleWVduDimensionsY - 2])
 
-            font_Fredoka_one_25 = pygame.font.SysFont('Fredoka One', 25, True, False)
-            font_Fredoka_one_65 = pygame.font.SysFont('Fredoka One', 65, True, False)
-            font_Alata_25 = pygame.font.SysFont('Alata', 25)
-            font_Alata_65 = pygame.font.SysFont('Alata', 65)
-            score_tracker = font_Alata_25.render("Score: " + str(game.score), True, colours.black)
-            text_game_over = font_Fredoka_one_65.render("Game Over", True, (255, 125, 0))
-            text_game_over1 = font_Fredoka_one_65.render("Press ESC", True, (255, 215, 0))
+            fontOpenSans = pygame.font.SysFont('sans', 35)
+            score_tracker = fontOpenSans.render("Score: " + str(game.score), True, colours.black)
 
-            #Restart game font button
-            restart_text = font_Alata_25.render("Restart with Esc", True, colours.black)
+
             window.surface.blit(score_tracker, [0, 0])
-            window.surface.blit(restart_text, [132, 480])
             if game.state == "gameover":
-                window.surface.blit(text_game_over, [20, 200])
-                window.surface.blit(text_game_over1, [25, 265])
+                pass
 
             pygame.display.flip()
             clock.tick(fps)

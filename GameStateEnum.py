783b12a963b35d83309669e92d2e2eab06877765
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-08-03 10:03:28 +0100

    Added INITIALISED and QUIT to GameStateEnum.py

--- GameStateEnum.py
+++ GameStateEnum.py
@@ -4,9 +4,15 @@ class GameStateEnum(Enum):
     GAME_OVER = 0
     STARTED = 1
     PAUSED = 2
+    INITIALISED = 3
+    QUIT = 4
     def gameOver(self):
         return self == self.GAME_OVER
     def paused(self):
         return self == self.PAUSED
     def gameStarted(self):
         return self == self.STARTED
+    def initialised(self):
+        return self == self.INITIALISED
+    def quit(self):
+        return self == self.QUIT

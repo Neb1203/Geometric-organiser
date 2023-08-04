fc93fcbb39d01a851fa171f517ed5abb1177863c
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-08-04 15:33:50 +0100

    Pause menu works with pygame menu

--- MainGameplay.py
+++ MainGameplay.py
@@ -8,138 +8,203 @@ from GameStateEnum import GameStateEnum
 import button
 from PIL import Image
 from CenterButton import CenterButton
+gameRunning = True
+w = Window()
+colours = Colours()
+cb = CenterButton()
+
+
+
+def start_game():
+    pygame.init()
+    game = MainGameplay()
+    game.run_game()
+    pygame.quit()
 class MainGameplay:
+
     def __init__(self):
-        w = Window()
-        colours = Colours()
-        cb = CenterButton()
 
-        scaleWVduDimensionsX = (int(w.vduDimensions[0]) / 500) * 20
-        scaleWVduDimensionsY = (int(w.vduDimensions[1]) / 400) * 20
 
-        game = Tetris(10, 20)
+        self.scaleWVduDimensionsX = (int(w.vduDimensions[0]) / 500) * 20
+        self.scaleWVduDimensionsY = (int(w.vduDimensions[1]) / 400) * 20
 
-        # Loop until the user clicks the close button.
-        gameRunning = True
-        clock = pygame.time.Clock()
-        fps = 25
-        counter = 0
+        self.game = Tetris(10, 20)
 
-        interval = 100
-        delay = 300
+        self.clock = pygame.time.Clock()
+        self.fps = 25
+        self.counter = 0
 
-        pressing_down = False
-        pressing_right = False
-        pressing_left = False
+        self.interval = 100
+        self.delay = 300
 
-        while gameRunning:
-            if game.figure is None:
-                game.newFigure()
-            counter += 1
-            if counter > 100000:
-                counter = 0
+        self.pressing_down = False
+        self.pressing_right = False
+        self.pressing_left = False
 
-            if counter % (fps // game.level // 2) == 0 or pressing_down:
-                if game.state.gameStarted():
-                    game.goDown()
+    def run_game(self):
+        game_running = True
+        while game_running:
+            if self.game.figure is None:
+                self.game.newFigure()
+            self.counter += 1
+            if self.counter > 100000:
+                self.counter = 0
 
-            for event in pygame.event.get():
+            if self.counter % (self.fps // self.game.level // 2) == 0 or self.pressing_down:
+                if self.game.state.gameStarted():
+                    self.game.goDown()
+
+            for event in pygame.event.get():  # Move this loop inside the main game loop
                 if event.type == pygame.QUIT:
-                    gameRunning = False
+                    game_running = False
 
-                if event.type == pygame.KEYDOWN: #Down keys for rotating
+                if event.type == pygame.KEYDOWN:  # Down keys for rotating
                     if event.key == pygame.K_q:
-                        game.rotateRight()
+                        self.game.rotateRight()
                     if event.key == pygame.K_e:
-                        game.rotateLeft()
+                        self.game.rotateLeft()
 
                     if event.key == pygame.K_s:
-                        pressing_down = True
+                        self.pressing_down = True
 
                     if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                        pygame.key.set_repeat(delay, interval)
-                        game.goSide(-1)
+                        pygame.key.set_repeat(self.delay, self.interval)
+                        self.game.goSide(-1)
 
                     if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                        pygame.key.set_repeat(delay, interval)
-                        game.goSide(1)
+                        pygame.key.set_repeat(self.delay, self.interval)
+                        self.game.goSide(1)
                     if event.key == pygame.K_SPACE:
-                        game.goSpace()
+                        self.game.goSpace()
                     if event.key == pygame.K_ESCAPE:
-                        initialState = game.state
+                        print("escape")
+                        # self.return_to_main_menu()
+                        # self.resume_game()
+                        initialState = self.game.state
+                        print(initialState)
                         if initialState == GameStateEnum.PAUSED:
-                            game.state = GameStateEnum.STARTED
+                            print("dumy")
+                            self.resume_game()
                         elif initialState == GameStateEnum.STARTED:
-                            game.state = GameStateEnum.PAUSED
-                        #game.__init__(10, 20)
+                            print("dum")
+                            self.pauseMenu()
 
-                    if event.key == pygame.K_p:
-                        pygame.quit()
+                        # game.__init__(10, 20)
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_s: #knows when i lift the down key up
-                    pressing_down = False
-                if event.key == pygame.K_d or pygame.K_RIGHT:
-                    pressing_right = False
-                if event.key == pygame.K_a or pygame.K_LEFT:
-                    pressing_left = False
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_s:  # Knows when I lift the down key up
+                            self.pressing_down = False
+                        if event.key == pygame.K_d or pygame.K_RIGHT:
+                            self.pressing_right = False
+                        if event.key == pygame.K_a or pygame.K_LEFT:
+                            self.pressing_left = False
+
+                # The rest of the code (drawing, button handling, etc.) remains the same
+                # ...
 
             w.surface.fill(colours.backgroundColour)
 
-            for i in range(game.height):
-                for j in range(game.width):
-                    pygame.draw.rect(w.surface, colours.gridColour, [game.x + scaleWVduDimensionsX * j, game.y + scaleWVduDimensionsY * i, scaleWVduDimensionsX, scaleWVduDimensionsY], 1)
-                    if game.field[i][j] > 0:
-                        pygame.draw.rect(w.surface, Figure.colors[game.field[i][j]],
-                                         [game.x + scaleWVduDimensionsX * j + 1, game.y + scaleWVduDimensionsY * i + 1, scaleWVduDimensionsX - 2, scaleWVduDimensionsY - 1])
-            if game.figure is not None:
+            for i in range(self.game.height):
+                for j in range(self.game.width):
+                    pygame.draw.rect(w.surface, colours.gridColour, [self.game.x + self.scaleWVduDimensionsX * j, self.game.y + self.scaleWVduDimensionsY * i, self.scaleWVduDimensionsX, self.scaleWVduDimensionsY], 1)
+                    if self.game.field[i][j] > 0:
+                        pygame.draw.rect(w.surface, Figure.colors[self.game.field[i][j]],
+                                         [self.game.x + self.scaleWVduDimensionsX * j + 1, self.game.y + self.scaleWVduDimensionsY * i + 1, self.scaleWVduDimensionsX - 2, self.scaleWVduDimensionsY - 1])
+            if self.game.figure is not None:
                 for i in range(4):
                     for j in range(4):
                         p = i * 4 + j
-                        if p in game.figure.image():
-                            pygame.draw.rect(w.surface, Figure.colors[game.figure.color],
-                                             [game.x + scaleWVduDimensionsX * (j + game.figure.x) + 1,
-                                              game.y + scaleWVduDimensionsY * (i + game.figure.y) + 1,
-                                              scaleWVduDimensionsX - 2, scaleWVduDimensionsY - 2])
+                        if p in self.game.figure.image():
+                            pygame.draw.rect(w.surface, Figure.colors[self.game.figure.color],
+                                             [self.game.x + self.scaleWVduDimensionsX * (j + self.game.figure.x) + 1,
+                                              self.game.y + self.scaleWVduDimensionsY * (i + self.game.figure.y) + 1,
+                                              self.scaleWVduDimensionsX - 2, self.scaleWVduDimensionsY - 2])
 
             fontOpenSans = pygame.font.SysFont('sans', 35)
-            score_tracker = fontOpenSans.render("Score: " + str(game.score), True, colours.black)
+            score_tracker = fontOpenSans.render("Score: " + str(self.game.score), True, colours.black)
             pauseResumeButton = fontOpenSans.render("Resume", True, colours.black)
 
             w.surface.blit(score_tracker, [0, 0])
-            if game.state.gameOver():
+            if self.game.state.gameOver():
                 print("gameState = gameOver")
-            if game.state.paused():
-                square_color = (255, 255, 255)  # Red color
-                square_size = 200
-                square_x = w.vduDimensions[0] // 2 - square_size // 2  # Center the square horizontally
-                square_y = w.vduDimensions[1] // 2 - square_size // 2 - 60
-
-                # pygame.draw.rect(w.surface, square_color, (square_x, square_y, square_size, square_size))
-
-                resumePath = "button images/Resume imagee.png"
-                restartPath = "button images/Restart buttonn.png"
-                mainMenuPath = "button images/Quit buttonn.png"
-
-                totalNumButtons = 2
-                margins = 20
-
-                resumeImage = pygame.image.load(resumePath).convert_alpha()
-                restartImage = pygame.image.load(restartPath).convert_alpha()
-                mainMenuImage = pygame.image.load(mainMenuPath).convert_alpha()
-
-                resume_button = button.Button(cb.centerButtonWidth(resumePath), cb.centerButtonHeight(resumePath, totalNumButtons, margins, 0), resumeImage, 1)
-                restartButton = button.Button(cb.centerButtonWidth(restartPath), cb.centerButtonHeight(restartPath, totalNumButtons, margins, 1), restartImage, 1)
-                mainMenuButton = button.Button(cb.centerButtonWidth(mainMenuPath), cb.centerButtonHeight(mainMenuPath, totalNumButtons, margins, 2), mainMenuImage, 1)
-
-                if resume_button.draw(w.surface):
-                    game.state = GameStateEnum.STARTED
-                if restartButton.draw(w.surface):
-                    game.__init__(10, 20)
-                    game.newFigure()
-                if mainMenuButton.draw(w.surface):
-                    print("Return to main menu button pressed")
+                self.return_to_main_menu()
+            if self.game.state.paused():
+                pass
+                # square_color = (255, 255, 255)  # Red color
+                # square_size = 200
+                # square_x = w.vduDimensions[0] // 2 - square_size // 2  # Center the square horizontally
+                # square_y = w.vduDimensions[1] // 2 - square_size // 2 - 60
+                #
+                # # pygame.draw.rect(w.surface, square_color, (square_x, square_y, square_size, square_size))
+                #
+                # resumePath = "button images/Resume imagee.png"
+                # restartPath = "button images/Restart buttonn.png"
+                # mainMenuPath = "button images/Quit buttonn.png"
+                #
+                # totalNumButtons = 2
+                # margins = 20
+                #
+                # resumeImage = pygame.image.load(resumePath).convert_alpha()
+                # restartImage = pygame.image.load(restartPath).convert_alpha()
+                # mainMenuImage = pygame.image.load(mainMenuPath).convert_alpha()
+                #
+                # resume_button = button.Button(cb.centerButtonWidth(resumePath), cb.centerButtonHeight(resumePath, totalNumButtons, margins, 0), resumeImage, 1)
+                # restartButton = button.Button(cb.centerButtonWidth(restartPath), cb.centerButtonHeight(restartPath, totalNumButtons, margins, 1), restartImage, 1)
+                # mainMenuButton = button.Button(cb.centerButtonWidth(mainMenuPath), cb.centerButtonHeight(mainMenuPath, totalNumButtons, margins, 2), mainMenuImage, 1)
+                #
+                # if resume_button.draw(w.surface):
+                #     self.game.state = GameStateEnum.STARTED
+                # if restartButton.draw(w.surface):
+                #     self.game.__init__(10, 20)
+                #     self.game.newFigure()
+                # if mainMenuButton.draw(w.surface):
+                #     print("Return to main menu button pressed")
             pygame.display.flip()
-            clock.tick(fps)
+            self.clock.tick(self.fps)
 
-        pygame.quit()
+            if self.game.state.gameOver() or self.game.state.paused():
+                game_running = False
+                gameRunning = False
+
+    def return_to_main_menu(self):
+        print("return to main menu")
+        self.pause_menu.disable()
+        self.pause_menu.clear()
+    def restartGame(self):
+        print("def return_to_main_menu")
+        self.pause_menu.disable()
+        self.pause_menu.clear()
+        self.game.state = GameStateEnum.STARTED
+        self.game.__init__(10, 20)
+        self.game.newFigure()
+        global gameRunning
+        gameRunning = False
+
+    def pauseMenu(self):
+        print("def pauseMenu")
+        self.game.state = GameStateEnum.PAUSED
+        # Create a pause menu
+        self.pause_menu = pygame_menu.Menu('Pause Menu', w.vduDimensions[0], w.vduDimensions[1], theme=w.mainTheme)
+
+        # Add items to the pause menu (e.g., resume and quit options)
+        self.pause_menu.add.button('Resume', self.resume_game)
+        self.pause_menu.add.button('Restart', self.restartGame)
+        self.pause_menu.add.button('Quit', self.return_to_main_menu)
+        # Run the pause menu
+        self.pause_menu.mainloop(w.surface)
+
+    def resume_game(self):
+        # print("def resume")
+        # if self.game.state.paused():
+        #     print("paused")
+        global gameRunning
+        gameRunning = True
+        self.game.state = GameStateEnum.STARTED
+        self.pause_menu.disable()
+        self.pause_menu.clear()
+        print(self.game.state)
+
+
+
+if __name__ == "__main__":
+    start_game()

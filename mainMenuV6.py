e8e55fac7e14dcd420de28b3ab4417def03cf1fe
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-08-10 17:22:48 +0100

    Continued menu building

--- mainMenuV6.py
+++ mainMenuV6.py
@@ -4,50 +4,49 @@ from Colours import Colours
 from CenterButton import CenterButton
 
 w = Window()
-c = Colours()
 cb = CenterButton()
+c = Colours()
+class ButtonV3():
+    def __init__(self, text):
+        color = (255, 255, 255)
 
-surface = w.surface
-color = (255, 255, 255)
+        self.color_light = (170, 170, 170)
+        self.color_dark = (100, 100, 100)
 
-color_light = (170, 170, 170)
-color_dark = (100, 100, 100)
+        self.width = w.vduDimensions[0]
+        self.height = w.vduDimensions[1]
 
-width = w.vduDimensions[0]
-height = w.vduDimensions[1]
+        smallFont = pygame.font.SysFont('Corbel', 35)
+        self.text = smallFont.render(text, True, color)
+    def draw(self, x, y, totalBut, margins):
+        surface = w.surface
+        surface.fill(c.backgroundColour)
+        self.buttonX = x
+        self.buttonY = y
+        self.mouse = pygame.mouse.get_pos()
 
-smallFont = pygame.font.SysFont('Corbel', 35)
+        mouseBoundrayX = cb.centerButtonWidth(self.buttonX) <= self.mouse[0] <= cb.centerButtonWidth(self.buttonX) + self.buttonX
+        mouseBoundrayY = cb.centerButtonHeight(self.buttonY, totalBut, margins, 0) <= self.mouse[1] <= cb.centerButtonHeight(self.buttonY, totalBut, margins, 0) + self.buttonY
 
-text = smallFont.render('Quit', True, color)
+        if mouseBoundrayX and mouseBoundrayY:
+            pygame.draw.rect(surface, self.color_light, [cb.centerButtonWidth(self.buttonX),
+                                                    cb.centerButtonHeight(self.buttonY, totalBut, margins, 0),
+                                                    self.buttonX, self.buttonY])
+        else:
+            pygame.draw.rect(surface, self.color_dark, [cb.centerButtonWidth(self.buttonX), cb.centerButtonHeight(self.buttonY, totalBut, margins, 0), self.buttonX, self.buttonY])
 
+        surface.blit(text, (cb.centerButtonWidth(self.buttonX) + 35, cb.centerButtonHeight(self.buttonY, totalBut, margins, 0) + self.buttonY / 4))
+ButtonV3 = ButtonV3("hello")
 while True:
-
     for ev in pygame.event.get():
-
         if ev.type == pygame.QUIT:
             pygame.quit()
-
         if ev.type == pygame.MOUSEBUTTONDOWN:
-
-            if mouseBoundrayX and mouseBoundrayY:
+            if ButtonV3.draw(500, 600, 0, 20).mouseBoundrayX and ButtonV3.draw(500, 600, 0, 20).mouseBoundrayY:
                 pygame.quit()
 
-    w.surface.fill(c.backgroundColour)
-    buttonX = 200
-    buttonY = 60
-    mouse = pygame.mouse.get_pos()
-    totalNumButtons = 1
-    margins = 20
+ButtonV3.draw(500, 600, 0, 20)
 
-    mouseBoundrayX = cb.centerButtonWidth(buttonX) <= mouse[0] <= cb.centerButtonWidth(buttonX) + buttonX
-    mouseBoundrayY = cb.centerButtonHeight(buttonY, totalNumButtons, margins, 0) <= mouse[1] <= cb.centerButtonHeight(buttonY, totalNumButtons, margins, 0) + buttonY
-
-    if  mouseBoundrayX and mouseBoundrayY:
-        pygame.draw.rect(surface, color_light, [cb.centerButtonWidth(buttonX), cb.centerButtonHeight(buttonY, totalNumButtons, margins, 0), buttonX, buttonY])
-    else:
-        pygame.draw.rect(surface, color_dark, [cb.centerButtonWidth(buttonX), cb.centerButtonHeight(buttonY, totalNumButtons, margins, 0), buttonX, buttonY])
-
-    surface.blit(text, (cb.centerButtonWidth(buttonX) + 35, cb.centerButtonHeight(buttonY, totalNumButtons, margins, 0) + buttonY / 4))
 
     # updates the frames of the game
-    pygame.display.update()
+pygame.display.update()

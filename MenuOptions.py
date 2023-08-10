b2f02e5cd93489c44aa69de75ccabac7ab4b82bc
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-08-10 17:21:53 +0100

    Simplified login and added validate

--- MenuOptions.py
+++ MenuOptions.py
@@ -1,12 +1,17 @@
 import requests
 from MainGameplay import MainGameplay
+from datetime import datetime, timedelta
+import json
+from tokenModifier import TokenModifier
+from removeSpeach import removeSpeach
 
 class menuOptions:
     def __init__(self):
         self.user_email = 'null'
         self.user_password = 'null'
         self.user_name = 'null'
-        self.user_token = 'null'
+        self.sessionToken = 'null'
+
     def setDifficulty(self, value, difficulty):
         # Do the job here
         # window(50)
@@ -32,9 +37,6 @@ class menuOptions:
     def selectAccount(self, data, value):
         self.selectedAccount = value
 
-    def loggedSignin(self):
-        if self.selectedAccount != None:
-            print(self.selectedAccount)
 
     def startGame(self):
         global gameModeSelected
@@ -55,32 +57,36 @@ class menuOptions:
         signup = {'email': self.user_email, 'password': self.user_password, 'user_name': self.user_name}
         self.response = requests.post('http://127.0.0.2:5000/player_details', params=signup)
 
+    def validate(self):
+        token = removeSpeach(self.sessionToken)
+        tokenModifier = TokenModifier()
+        tokenTup = {'sessionToken': token,}
+        response = requests.get('http://127.0.0.2:5000/validate', params=tokenTup)
+        decoded = response.content.decode('utf-8')
+        print(decoded)
 
-    def login(self):
-        print("def login running")
-        login = {'email': self.user_email, 'password': self.user_password}
+        data = json.loads(decoded)
+        date_time_str = data[0]
 
-        response = requests.get('http://127.0.0.2:5000/player_details', params=login)
+        responseTime = datetime.strptime(date_time_str, "%Y-%m-%dT%H:%M:%S")
+        new_date = responseTime + timedelta(days=30)
+        if new_date <= datetime.now():
+            existing_session_ids = tokenModifier.read_session_ids()
 
-        if response.status_code == 200:
-            try:
-
-                response_content = response.content.decode('utf-8')  # Decode the response content from bytes to a string
-                if response_content == 'null': # Handle the case when the response is 'null'
-                    print('No account with details')
+            if existing_session_ids is not None:  # Replace with the session ID you want to remove
+                if token in existing_session_ids:
+                    existing_session_ids.remove(token)
+                    tokenModifier.write_session_ids(existing_session_ids)
                 else:
-                    jsonResponse = response.json()  # Parse the response content as JSON
-                    print("Response JSON:", jsonResponse)
-
-                    if isinstance(jsonResponse, dict):
-                        user_name = jsonResponse.get('username')
-                        print("Extracted username:", user_name)
-                        self.user_token = jsonResponse.get('token')
-                        print("Extracted token:", self.user_token)
-                        return self.user_token
-                    else: # Handle the case when the response is not a JSON object
-                        print("The data isn't in JSON")
-            except ValueError: # Handle the case when the response content cannot be parsed as JSON
-                pass
-        else: # Handle the case when the request was not successful
-            pass
+                    print("Session ID not found in the file.")
+        else:
+            print("valid")
+    def cloudLogin(self):
+        print("def cloudLogin running")
+        login = {'email': self.user_email, 'password': self.user_password}
+        response = requests.post('http://127.0.0.2:5000/authenticate', params=login)
+        if response.status_code == 200:
+                response_content = response.content.decode('utf-8')
+                self.sessionToken = response_content
+        else:
+            print("code not equal to 200")
